c8546ddbbee094389eaf97a6fdcf64f13a9e8ac4
janae.wel <janae.wel@gmail.com>
2022-08-17 15:38:00 -0400

    started votes

--- models/index.js
+++ models/index.js
@@ -1,12 +1,36 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
-// create one-to-many associations
+// create one-to-many association
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
+
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = {User, Post};
+// create many-to-many association
+// association to see which users commented on a post:
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+// association to see which posts a user commented on
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = {User, Post, Vote};

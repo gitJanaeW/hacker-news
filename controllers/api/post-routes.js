1a1c23204b2c7ba91e3b5953f606cccfdca98ac4
janae.wel <janae.wel@gmail.com>
2022-08-24 14:44:59 -0400

    still fixing upvote bug

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -83,7 +83,7 @@ router.put('/upvote', (req, res) => {
   // if a session exists, put upvote
   if (req.session) {
     // pass session id along with all destructured properties on req.body
-    Post.upvote({...req.body, user_id: req.session.user_id})
+    Post.upvote({...req.body, user_id: req.session.user_id}, {Vote, Comment, User})
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
         console.log(err);
--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,41 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
+  // static: indicates here that the "upvote" method is based on the Post class and not based on indivdual Post objs
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id
+    })
+    // then find the post we just voted on
+    .then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          // use raw MySQL to query the amount of votes the post has and add it to the post obj under the name `vote_count`
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: models.User,
+              attributes: ['username']
+            }
+          }
+        ]
+      });
+    });
+  }
+}
 
 // create fields/columns for Post model
 Post.init(
